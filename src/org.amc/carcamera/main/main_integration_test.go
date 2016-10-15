e2e811d8ad3466dff6ca7b09f4dd26d3fd77a2c8
Adrian McLaughlin <subwoofer359@gmail.com>
2016-10-15 12:24:10 +0100

    disable integration test

--- src/org.amc/carcamera/main/main_integration_test.go
+++ src/org.amc/carcamera/main/main_integration_test.go
@@ -1,33 +1,33 @@
 package main
-
-import (
-	"testing"
-	"org.amc/carcamera/storageManager"
-	"os/exec"
-	"time"
-	"log"
-)
-
-func TestIntegration(t *testing.T) {
-	WebCamApp := &CameraCommandImpl {
-		command: "/usr/bin/avconv",
-		args: []string{"-f", "video4linux2", "-i", "/dev/video0", "-t", "5", "-y"},
-		storageManager: storageManager.New(),
-		exec: exec.Command,
-	}
-	
-	WebCamApp.storageManager.SetWorkDir("/tmp")
-	WebCamApp.storageManager.Init()
-	
-	
-	
-	for t := 0; t < 2; t++ {
-		runner := New(7 * time.Second)
-		runner.add(WebCamApp)
-		err := runner.Start()
-		if err != nil && err.Error() != "completed" {
-			log.Fatal(err)	
-		}
-	}
-}
-
+//
+//import (
+//	"testing"
+//	"org.amc/carcamera/storageManager"
+//	"os/exec"
+//	"time"
+//	"log"
+//)
+//
+//func TestIntegration(t *testing.T) {
+//	WebCamApp := &CameraCommandImpl {
+//		command: "/usr/bin/avconv",
+//		args: []string{"-f", "video4linux2", "-i", "/dev/video0", "-t", "5", "-y"},
+//		storageManager: storageManager.New(),
+//		exec: exec.Command,
+//	}
+//	
+//	WebCamApp.storageManager.SetWorkDir("/tmp")
+//	WebCamApp.storageManager.Init()
+//	
+//	
+//	
+//	for t := 0; t < 2; t++ {
+//		runner := New(7 * time.Second)
+//		runner.add(WebCamApp)
+//		err := runner.Start()
+//		if err != nil && err.Error() != "completed" {
+//			log.Fatal(err)	
+//		}
+//	}
+//}
+//
