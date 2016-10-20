a683c494f429fafd153c4e0942af314a43358c55
Adrian McLaughlin <subwoofer359@gmail.com>
2016-10-20 19:13:42 +0100

    alter to simple mapping json to map

--- src/org.amc/carcamera/config/configuration.go
+++ src/org.amc/carcamera/config/configuration.go
@@ -1,45 +1,20 @@
 package config
 
 import (
-	//C "org.amc/carcamera/constants"
 	"encoding/json"
 	"io"
 	"log"
-	"reflect"
 )
 
-type (
-	context struct {
-		COMMAND string `json:"COMMAND"`
-		WORKDIR string `json:"WORKDIR"`
-		TIMEOUT string `json:"TIMEOUT"`
-		VIDEOLENGTH string `json:"VIDEOLENGTH"`
-		PREFIX string `json:"PREFIX"`
-		SUFFIX string `json:"SUFFIX"`
-		MINFILESIZE string `json:"MINFILESIZE"`
-		MAXNOOFFILES string `json:"MAXNOOFFILES`
-	}
-)
-
-func readConfigurationFile(jsonReader io.Reader) (map[int] string, error) {
-	var (
-		c context
-	)
+func readConfigurationFile(jsonReader io.Reader) (map[string] interface{}, error) {
+	var context map[string] interface{}
 	
-	
-	err := json.NewDecoder(jsonReader).Decode(&c)
+	err := json.NewDecoder(jsonReader).Decode(&context)
 	
 	if err != nil {
 		log.Println("Decoding Error")
 		return nil, err
 	}
 	
-	config := make(map[int] string)
-	
-	val := reflect.Indirect(reflect.ValueOf(c))
-	for i := 0 ; i < val.NumField(); i = i + 1 {
-		log.Println(val.Type().Field(i).Name)	
-	}
-	
-	return config, nil
+	return context, nil
 }
