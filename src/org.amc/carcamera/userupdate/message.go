fffdb7ecd3bc0f457ce695f427ba6bc83c8d198b
Adrian McLaughlin <subwoofer359@gmail.com>
2017-03-20 10:40:33 +0000

    create message package and interface

---
+++ src/org.amc/carcamera/userupdate/message.go
@@ -0,0 +1,46 @@
+package userupdate 
+
+import (
+
+)
+
+type messageService interface {
+	Init() error
+	Error(message string)
+	Started()
+	
+}
+
+type Message struct {
+	services []messageService
+}
+
+func (m Message) Error(message string) {
+	for _, service := range m.services {
+		service.Error(message)
+	}
+}
+
+/*
+ * Initialising the Messaging services
+ */
+func (m Message) Init() error {
+	for _, service := range m.services {
+		err := service.Init()
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (m Message) Started() {
+	for _, service := range m.services {
+		service.Started()
+	}
+}
+
+
+func (m *Message) AddService(service messageService) {
+	m.services = append(m.services, service)
+} 
---
+++ src/org.amc/carcamera/userupdate/message_test.go
@@ -0,0 +1,113 @@
+package userupdate
+
+import (
+    "testing"
+    "errors"
+)
+
+type testService struct {
+	initCalled bool
+	started bool
+	errMessage string
+}
+
+func (testS *testService) Init() error {
+	testS.initCalled = true
+	return nil
+}
+
+func (testS *testService) Started() {
+	testS.started = true
+}
+
+func (testS *testService) Error(message string) {
+	testS.errMessage = message
+}
+
+
+
+type testServiceFail struct {
+	initCalled bool
+	errMessage string
+	started bool
+	
+}
+
+func (testS *testServiceFail) Init() error {
+	
+	return errors.New("Test Error")
+}
+
+func (testS *testServiceFail) Error(message string) {
+	testS.errMessage = message
+}
+
+func (testS *testServiceFail) Started() {
+	testS.started = true
+}
+
+
+func TestMessageAddService(t *testing.T) {
+	message := new(Message)
+	tService := new(testService)
+	
+	if len(message.services) != 0 {
+		t.Error("The list of services should be empty")
+	}
+	
+	message.AddService(tService)
+	
+	if len(message.services) != 1 {
+		t.Error("The list of services should contain only one service")
+	}
+}
+
+func TestMessageInit(t *testing.T) {
+	message := new(Message)
+	tService := new(testService)
+	message.AddService(tService)
+	
+	message.Init()
+	
+	if tService.initCalled != true {
+		t.Error("Init should have been called on the service")
+	}
+}
+
+func TestMessageInitThrowsError(t *testing.T) {
+	message := new(Message)
+	tService := new(testServiceFail)
+	message.AddService(tService)
+	
+	err := message.Init()
+	
+	if err == nil {
+		t.Error("Error should have been thrown on Init() call")
+	}
+}
+
+func TestSendErrorMessage(t *testing.T) {
+	message := new(Message)
+	tService := new(testService)
+	errorMsg := "error!!!"
+	
+	message.AddService(tService)
+	
+	message.Error(errorMsg)
+	
+	if tService.errMessage != errorMsg {
+		t.Error("Error message not passed to service")
+	}
+}
+
+func TestStarted(t *testing.T) {
+	message := new(Message)
+	tService := new(testService)
+	message.AddService(tService)
+	
+	message.Started()
+	
+	if tService.started == false {
+		t.Error("Service not started")
+	}
+}
