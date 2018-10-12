685f9239866acd8f888c2186a6e230d41d65f542
Adrian McLaughlin <adrian@adrianmclaughlin.ie>
2018-10-12 11:18:06 +0100

    fix filename typos

--- src/org.amc/carcamera/main/PowerControl.go
+++ src/org.amc/carcamera/main/PowerControl.go
@@ -1,6 +1,12 @@
 package main
 
-import "org.amc/carcamera/warning"
+import (
+	"errors"
+
+	"org.amc/carcamera/warning"
+)
+
+var ErrPowerFault = errors.New("Power Control Interrupt")
 
 type PowerControl interface {
 	SetGPIO(p *PowerControlImpl, gpio warning.Gpio)
