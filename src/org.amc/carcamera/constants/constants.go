eece6c03af01f96df1b47cecd9b19a6aaeaafc55
Adrian McLaughlin <adrian@adrianmclaughlin.ie>
2018-10-16 11:57:57 +0100

    add bluetooth configuration option

--- src/org.amc/carcamera/constants/constants.go
+++ src/org.amc/carcamera/constants/constants.go
@@ -1,21 +1,23 @@
 package constants
 
-
 import (
 	"os"
 )
+
 const (
-	COMMAND = "COMMAND" //COMMAND the external program to execute
-	WORKDIR = "WORKDIR" //WORKDIR directory to write files to
-	TIMEOUT = "TIMEOUT" //TIMEOUT kill the spawned process after set time out
-	VIDEOLENGTH = "VIDEOLENGTH" //VIDEOLENGTH the length of the video to record (MILLISECONDS)
-	PREFIX = "PREFIX" //PREFIX file name prefix
-	SUFFIX = "SUFFIX" //SUFFIX file extension
-	MINFILESIZE = "MINFILESIZE" //MINFILESIZE Mininum file size
+	BLUETOOTH    = "BLUETOOTH"    //BLUETOOTH to use bluetooth or not
+	COMMAND      = "COMMAND"      //COMMAND the external program to execute
+	WORKDIR      = "WORKDIR"      //WORKDIR directory to write files to
+	TIMEOUT      = "TIMEOUT"      //TIMEOUT kill the spawned process after set time out
+	VIDEOLENGTH  = "VIDEOLENGTH"  //VIDEOLENGTH the length of the video to record (MILLISECONDS)
+	PREFIX       = "PREFIX"       //PREFIX file name prefix
+	SUFFIX       = "SUFFIX"       //SUFFIX file extension
+	MINFILESIZE  = "MINFILESIZE"  //MINFILESIZE Mininum file size
 	MAXNOOFFILES = "MAXNOOFFILES" //MAXNOOFFILES maximum no of files in the WORKDIR
-	OPTIONS = "OPTIONS" //OPTIONS command line options
-	
-	GAP_SERVICE_NAME = "GAPSERVICENAME" // Name of GAP bluetooth service
+	OPTIONS      = "OPTIONS"      //OPTIONS command line options
+	OFF          = "off"          // off setting
+
+	GAP_SERVICE_NAME  = "GAPSERVICENAME"  // Name of GAP bluetooth service
 	GATT_SERVICE_NAME = "GATTSERVICENAME" // Name of GATT bluetooth service
 )
 
